0c7ef27f6a93c12f35f9ac6cfedbc4ef1c88003e
r.yurmanov@gmail.com <r.yurmanov@gmail.com>
2020-06-06 21:38:15 +0300

    final

--- src/main/java/sberbank/pages/MainPage.java
+++ src/main/java/sberbank/pages/MainPage.java
@@ -22,7 +22,13 @@ public class MainPage extends BasePage {
 
     public void checkCookie() {
         if (!BaseSteps.getWebDriver().findElements(By.xpath("//div[@class='cookie-warning cookie-warning_show']")).isEmpty()) {
-        clickElement(cookieClose);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            moveElement(cookieClose);
+            clickElement(cookieClose);
         }
     }
 
