b8eec58eced0fcb0fbb65f6b6760d1f685cc127d
r.yurmanov@gmail.com <r.yurmanov@gmail.com>
2020-06-06 19:01:03 +0300

    final

--- src/main/java/sberbank/pages/MainPage.java
+++ src/main/java/sberbank/pages/MainPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import sberbank.steps.BaseSteps;
-import sberbank.utils.AllureUtils;
 
 public class MainPage extends BasePage {
     @FindBy(xpath = "//a[@class='cookie-warning__close']")
